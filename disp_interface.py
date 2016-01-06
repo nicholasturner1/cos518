ed2c34fd1063776ffee98f16b983778e1885c89b
Yuanzhi Li <yuanzhili92@gmail.com>
2016-01-06 12:27:21 -0500

    add social thing

--- disp_interface.py
+++ disp_interface.py
@@ -2,6 +2,10 @@
 
 import numpy as np
 
+
+def assign_topic( email_x_topic_matrix, num_ids):
+    return column_cluster_ids( email_x_topic_matrix, num_ids )
+
 def assign_emails( email_x_topic_matrix, num_ids):
     return row_cluster_ids( email_x_topic_matrix, num_ids )
 
@@ -21,4 +25,24 @@ def row_cluster_ids( assign_matrix, num_ids ):
         row_assignment = np.argsort( assign_matrix[i,:] )[::-1][:num_ids]
         assignments.append( row_assignment )
 
+    return assignments
+
+
+
+def column_cluster_ids( assign_matrix, num_ids ):
+    '''
+        Takes a matrix which makes a soft assignment of column to
+        a cluster (either content topic or social topic) and returns
+        an explicit hard assignment of each column to a {num_ids} number
+        of clusters
+        '''
+    
+    rows, cols = assign_matrix.shape
+    
+    assignments = []
+    
+    for i in range(cols):
+        col_assignment = np.argsort( assign_matrix[:,i] )[::-1][-num_ids:]
+        assignments.append( col_assignment )
+    
     return assignments
